0f33ec8a2271c55dbf7e52c758f23248053d9a72
Erik Ehresmann <erik.ehresmann9543@gmail.com>
2020-07-28 07:44:25 -0500

    Game controller Get and Delete functionality added

--- src/main/java/com/github/ede/ConnectFourGame/controllers/GameController.java
+++ src/main/java/com/github/ede/ConnectFourGame/controllers/GameController.java
@@ -6,12 +6,10 @@ import com.github.ede.ConnectFourGame.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/games")
@@ -21,9 +19,28 @@ public class GameController {
     GameRepository gameRepository;
 
     @PostMapping
-    public ResponseEntity<Game> addGame(@Valid @RequestBody Game game){
+    public ResponseEntity<Game> addGame(@Valid @RequestBody Game game) {
         Game connectFourGame = gameRepository.save(game);
         return ResponseEntity.ok(game);
     }
 
+    @GetMapping("/game/{gameId}")
+    public ResponseEntity<Game> getGame(@PathVariable("gameId") Long gameId) {
+        Game connectFourGame = gameRepository.findById(gameId).orElse(null);
+        assert connectFourGame != null;
+        return ResponseEntity.ok(connectFourGame);
+    }
+
+    @DeleteMapping("/game/{gameId}")
+    public ResponseEntity<String> deleteGame(@PathVariable("gameId") Long gameId) {
+        //deletes game record based off of gameId passed
+        gameRepository.deleteById(gameId);
+        //If the delete was not completed returns a bad response
+        if (gameRepository.findById(gameId).orElse(null) != null) {
+            return (ResponseEntity<String>) ResponseEntity.badRequest();
+        } else {
+            return ResponseEntity.ok("Your Game was deleted");
+        }
+
+    }
 }
